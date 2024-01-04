6a0c93b54cd2e25a4e183c5378c38693a9099eff
vozarv <vozar98@hotmail.com>
2024-01-04 20:07:31 +0100

    [ADD] Chapter 5: normals, multiple objects

--- main.cpp
+++ main.cpp
@@ -1,73 +1,64 @@
-#include <iostream>
-#include <fstream>
-#include <stdlib.h>
-#include <math.h>
 #include "float.h"
-#include "ray,hpp"
+#include "hitable_list.hpp"
+#include "sphere.hpp"
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+
 
 using namespace std;
 
 
 
-float hit_sphere(const vec3& center, float radius, const ray& r){
-    vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = 2.0f * dot(oc, r.direction());
-    float c = dot(oc, oc) - radius * radius;
-    float discriminant = b * b - 4.0f * a * c;
-    if (discriminant < 0){
-        return -1.0;
-    }
-    else {
-        return (-b -sqrt(discriminant)) / (2.0f * a);
-    }
-}
+vec3 color(const ray &r, hitable *world) {
 
-vec3 color(const ray& r){
-    float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
-    if( t > 0.0 ) {
-        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-        return 0.5 * N + 1.0;
-    }
-
+  hit_record rec;
+  if (world->hit(r, 0.0, MAXFLOAT, rec)) {
+    return 0.5 * (rec.normal + 1.0);
+  } else {
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5f * (unit_direction.y() + 1.0f);
-    return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+  }
 }
 
-int main(){
 
-    ofstream file("output.ppm");
+int main() {
 
-    vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
-    vec3 horizontal(4.0f, 0.0f, 0.0f);
-    vec3 vertical(0.0f, 2.0f, 0.0f);
-    vec3 origin(0.0f, 0.0f, 0.0f);
-    
+  ofstream file("../output.ppm");
 
-    int nx = 200;
-    int ny = 100;
+  vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
+  vec3 horizontal(4.0f, 0.0f, 0.0f);
+  vec3 vertical(0.0f, 2.0f, 0.0f);
+  vec3 origin(0.0f, 0.0f, 0.0f);
 
-    file << "P3\n" << nx << " " << ny << "\n255\n";
+    hitable *list[2];
+    list[0] = new sphere(vec3(0,0,-1), 0.5);
+    list[1] = new sphere(vec3(0,-100.5,-1), 100);
+    hitable *world = new hitable_list(list, 2);
 
-    for(int j = ny - 1; j >= 0; j--){
-        for(int i = 0; i < nx; i++){
-            
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
+  int nx = 200;
+  int ny = 100;
 
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            vec3 col = color(r);
-            //vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2f);
+  file << "P3\n" << nx << " " << ny << "\n255\n";
 
-            int ir = int(255.99*col[0]);
-            int ig = int(255.99*col[1]);
-            int ib = int(255.99*col[2]);
-            file << ir << " " << ig << " " << ib << "\n";
-        }
+  for (int j = ny - 1; j >= 0; j--) {
+    for (int i = 0; i < nx; i++) {
+
+      float u = float(i) / float(nx);
+      float v = float(j) / float(ny);
+
+      ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+      vec3 col = color(r, world);
+
+      int ir = int(255.99 * col[0]);
+      int ig = int(255.99 * col[1]);
+      int ib = int(255.99 * col[2]);
+      file << ir << " " << ig << " " << ib << "\n";
     }
+  }
 
-    file.close();
-
-
+  file.close();
 }
