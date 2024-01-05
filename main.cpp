8b6bb7ee2749aa20ddab9b81f4053731c21f0808
vozarv <vozar98@hotmail.com>
2024-01-05 09:27:03 +0100

    [ADD] Chapter 7: diffuse materials

--- main.cpp
+++ main.cpp
@@ -10,17 +10,6 @@
 
 using namespace std;
 
-vec3 color(const ray &r, hitable *world) {
-
-  hit_record rec;
-  if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-    return 0.5 * (rec.normal + 1.0);
-  } else {
-    vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
-  }
-}
 
 double random_double(double min, double max) {
   std::random_device rd;
@@ -28,6 +17,30 @@ double random_double(double min, double max) {
   std::uniform_real_distribution<double> distribution(min, max);
   return distribution(gen);
 }
+
+vec3 random_in_unit_sphere() {
+  vec3 p;
+  do {
+    p = 2.0 * vec3(random_double(-1.0, 1.0), random_double(-1.0, 1.0), random_double(-1.0, 1.0)) - vec3(1.0, 1.0, 1.0);
+  } while (p.length2() >= 1.0);
+  return p;
+}
+
+vec3 color(const ray &r, hitable *world) {
+
+  hit_record rec;
+  if (world->hit(r, 0.00001, MAXFLOAT, rec)) {
+    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+    return 0.5 * color(ray(rec.p, target - rec.p), world);
+
+  } else {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+  }
+}
+
+
 
 int main() {
 
@@ -47,11 +60,14 @@ int main() {
 
   int nx = 200;
   int ny = 100;
-  int ns = 100;
+  int ns = 10;
 
   file << "P3\n" << nx << " " << ny << "\n255\n";
 
   for (int j = ny - 1; j >= 0; j--) {
+
+    std::clog << "\rScanlines remaining: " << j << ' ' << std::flush;
+    
     for (int i = 0; i < nx; i++) {
 
       vec3 col(0, 0, 0);
@@ -66,13 +82,15 @@ int main() {
       }
 
       col /= float(ns);
-
+      col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
       int ir = int(255.99 * col[0]);
       int ig = int(255.99 * col[1]);
       int ib = int(255.99 * col[2]);
       file << ir << " " << ig << " " << ib << "\n";
     }
   }
+
+  std::clog << "\rDone.                 \n";
 
   file.close();
 }
