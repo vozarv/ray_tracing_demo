afc2a8f2a48230fdb2ad40f5fe4283d9b9ba79ed
vozarv <vozar98@hotmail.com>
2024-01-17 19:16:08 +0100

    [WIP] These are OK

--- utils.hpp
+++ utils.hpp
@@ -11,7 +11,6 @@ double random_double(double min, double max) {
 
 vec3 random_in_unit_sphere() {
 
-
   double r = random_double(0.0, 1.0);
   double azimuth = random_double(0.0, 2.0 * M_PI);
   double elevation = random_double(0.0, M_PI);
@@ -23,7 +22,6 @@ vec3 random_in_unit_sphere() {
 }
 
 vec3 random_in_unit_disk() {
-  
 
   double r = random_double(0.0, 1.0);
   double azimuth = random_double(0.0, 2.0 * M_PI);
@@ -31,4 +29,13 @@ vec3 random_in_unit_disk() {
   vec3 p(r * cos(azimuth), r * sin(azimuth), 0.0);
 
   return p;
+}
+
+void get_sphere_uv(const vec3 &p, float &u, float &v) {
+  
+  float phi = atan2(p.z(), p.x());
+  float theta = asin(p.y());
+
+  u = 1 - (phi + M_PI) / (2.0 * M_PI);
+  v = (theta + M_PI / 2) / M_PI;
 }
